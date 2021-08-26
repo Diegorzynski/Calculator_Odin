48e8b92eb2cce56211abaaabca7d2589d380b952
diegorzynski <diegorzynski@hotmail.com>
2021-08-26 11:43:15 +0200

    working adding multiple operations

--- app.js
+++ app.js
@@ -1,37 +1,46 @@
 const numbers = Array.from(document.getElementsByClassName('numbers'));
-numbers.forEach(el => el.addEventListener('click', showOnScreen));
-let initial_screen = document.getElementById('screen')
+numbers.forEach(el => el.addEventListener('click', assignNum));
+let screen = document.getElementById('screen')
+const operations = Array.from(document.getElementsByClassName('ops'));
+operations.forEach(el => el.addEventListener('click', operation));
+const equal = Array.from(document.getElementsByClassName('equal'));
+equal.forEach(el => el.addEventListener('click', operate))
 
 let firstNum = null;
+let secondNum = null;
+let operator = '';
 
 
-function showOnScreen(e){ //shows on screesna and assign to input to be later divided into different numbers
-initial_screen.innerHTML += e.target.id;
-if(firstNum == null) {
-    firstNum = e.target.id;
+function assignNum(e){ 
+screen.innerText += e.target.id;
+if(operator.length == 0) {
+    firstNum == null ? firstNum = e.target.id : firstNum += e.target.id
 } else {
-    firstNum += e.target.id;
+    secondNum == null ? secondNum = e.target.id : secondNum += e.target.id;
    }
 };
 
 
-function operate(operator,num_1, num_2){
-
-
-}
-
-function add(){
+function operation(e){
+if(operator.length == 0){
+operator = e.target.id;
+} else {
 
 }
-
-function subtract(){
-
+screen.innerText += e.target.id;
 }
 
-function multiply(){
 
-}
+function operate(){
+    result = '';
+    firstNum = parseInt(firstNum, 10);
+    secondNum = parseInt(secondNum, 10);
+    if(operator == '+' || operator == '-') {
+       result = operator == '+' ? firstNum + secondNum : firstNum - secondNum;
+    } else if (operator == '*' || operator == '/') {
+        result = operator == '*' ? firstNum * secondNum : firstNum / secondNum;
+    }
+    firstNum = result;
+    screen.innerText = result;
+};
 
-function divide(){
-
-}
